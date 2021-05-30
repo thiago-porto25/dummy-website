6ce8d9a081d2a9ebfb640b3bd5a3d793cbfbda10
thiago-porto25 <thiago_porto25@hotmail.com>
2021-05-30 12:29:03 -0300

    create the full nextSlide functions

--- src/index.js
+++ src/index.js
@@ -5,26 +5,32 @@ const gallery = (function () {
   const _nextSlide = () => {
     const _songsFrame = document.querySelector('.songsFrame')
     const _displayed = document.querySelector('.displayed')
-    const _displayedId = _displayed.getAttribute('data-id')
-    const _nextSongId = parseInt(_displayedId) + 1
+    const _displayedId = parseInt(_displayed.getAttribute('data-id'))
+    let _nextSongId = _displayedId + 1
+    let _param = 40 * _displayedId
+
+    if (_displayedId === 6) {
+      _nextSongId = 1
+      _param = 0
+    }
+
     const _nextSong = document.querySelector(`[data-id='${_nextSongId}']`)
 
     _displayed.classList.toggle('displayed')
     _nextSong.classList.toggle('displayed')
-    const _param = 40 * parseInt(_displayedId)
 
     _songsFrame.style.transform = `translateX(-${_param}rem)`
   }
   const _prevSlide = () => {
     const _songsFrame = document.querySelector('.songsFrame')
     const _displayed = document.querySelector('.displayed')
-    const _displayedId = _displayed.getAttribute('data-id')
-    const _prevSongId = parseInt(_displayedId) - 1
+    const _displayedId = parseInt(_displayed.getAttribute('data-id'))
+    const _prevSongId = _displayedId - 1
     const _prevSong = document.querySelector(`[data-id='${_prevSongId}']`)
 
     _displayed.classList.toggle('displayed')
     _prevSong.classList.toggle('displayed')
-    const _param = 40 * parseInt(_prevSongId - 1)
+    const _param = 40 * (_prevSongId - 1)
 
     _songsFrame.style.transform = `translateX(-${_param}rem)`
   }
