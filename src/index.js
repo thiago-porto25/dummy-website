128ec431752b38b28a42c308225b5cfb264b46dd
thiago-porto25 <thiago_porto25@hotmail.com>
2021-06-02 12:30:17 -0300

    create, style and add js to the songs gallery nav

--- src/index.js
+++ src/index.js
@@ -3,13 +3,16 @@
 const gallery = (function galleryHandler() {
   const _leftArrow = document.querySelector('.left')
   const _rightArrow = document.querySelector('.right')
+  const _songsNavItems = document.querySelectorAll('.songsNavItem')
 
   const _nextSlide = () => {
     const _width = document.querySelector('.songsContainer').clientWidth
     const _songsFrame = document.querySelector('.songsFrame')
 
     const _displayed = document.querySelector('.displayed')
+
     const _displayedId = parseInt(_displayed.getAttribute('data-id'), 10)
+    const _songsNavItemDisplayed = document.querySelector('.songsNavDisplayed')
 
     let _nextSongId = _displayedId + 1
     let _param = _width * _displayedId
@@ -24,15 +27,24 @@ const gallery = (function galleryHandler() {
     }
 
     const _nextSong = document.querySelector(`[data-id='${_nextSongId}']`)
+    const _nextSongsNavItem = document.querySelector(
+      `[data-songsNav='${_nextSongId}']`
+    )
     _displayed.classList.toggle('displayed')
+    _songsNavItemDisplayed.classList.toggle('songsNavDisplayed')
     _nextSong.classList.toggle('displayed')
+    _nextSongsNavItem.classList.toggle('songsNavDisplayed')
     _songsFrame.style.transform = `translateX(-${_param}px)`
   }
   const _prevSlide = () => {
     const _width = document.querySelector('.songsContainer').clientWidth
     const _songsFrame = document.querySelector('.songsFrame')
+
+    const _songsNavItemDisplayed = document.querySelector('.songsNavDisplayed')
+
     const _displayed = document.querySelector('.displayed')
     const _displayedId = parseInt(_displayed.getAttribute('data-id'), 10)
+
     let _prevSongId = _displayedId - 1
     let _param = _width * (_prevSongId - 1)
 
@@ -43,17 +55,43 @@ const gallery = (function galleryHandler() {
     }
 
     const _prevSong = document.querySelector(`[data-id='${_prevSongId}']`)
+    const _prevSongsNavItem = document.querySelector(
+      `[data-songsNav='${_prevSongId}']`
+    )
     _displayed.classList.toggle('displayed')
+    _songsNavItemDisplayed.classList.toggle('songsNavDisplayed')
     _prevSong.classList.toggle('displayed')
+    _prevSongsNavItem.classList.toggle('songsNavDisplayed')
+    _songsFrame.style.transform = `translateX(-${_param}px)`
+  }
+  const _navigateSlide = (e) => {
+    const _width = document.querySelector('.songsContainer').clientWidth
+    const _songsFrame = document.querySelector('.songsFrame')
+
+    const _displayed = document.querySelector('.displayed')
+    const _songsNavItemDisplayed = document.querySelector('.songsNavDisplayed')
+
+    const _nextSongId = parseInt(e.target.getAttribute('data-songsNav'), 10)
+    const _param = _width * (_nextSongId - 1)
+
+    const _nextSong = document.querySelector(`[data-id='${_nextSongId}']`)
+    const _nextSongsNavItem = document.querySelector(
+      `[data-songsNav='${_nextSongId}']`
+    )
+    _displayed.classList.toggle('displayed')
+    _songsNavItemDisplayed.classList.toggle('songsNavDisplayed')
+    _nextSong.classList.toggle('displayed')
+    _nextSongsNavItem.classList.toggle('songsNavDisplayed')
     _songsFrame.style.transform = `translateX(-${_param}px)`
   }
 
-  _leftArrow.addEventListener('click', () => {
-    _prevSlide()
-  })
-  _rightArrow.addEventListener('click', () => {
-    _nextSlide()
-  })
+  _leftArrow.addEventListener('click', _prevSlide)
+  _rightArrow.addEventListener('click', _nextSlide)
+  _songsNavItems.forEach((item) =>
+    item.addEventListener('click', (e) => {
+      _navigateSlide(e)
+    })
+  )
 
   setInterval(() => _nextSlide(), 5000)
 })()
